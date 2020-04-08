838a9972bad5d673023a46569b20231f1ee7119c
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-08 17:23:30 -0500

    A new property to the usermodel was added to the database and some data was inserted in the database.

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,11 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    is_admin = models.BooleanField(default=False)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
