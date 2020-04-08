a749ac73028c3c8ef0877a3097972aa29ce163d7
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-08 18:34:18 -0500

    The login function was created.

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -6,13 +6,15 @@ from platzigram import views as local_views
 from posts import views as post_views
 from django.contrib import admin
 from django.conf import settings
+from users import views as users_views
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('hello-world/', local_views.hello_world),
-    path('sorted/', local_views.sort_integers),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi),
-    path('posts/', post_views.list_post)
+    path('hello-world/', local_views.hello_world, name = 'hello_world'),
+    path('sorted/', local_views.sort_integers, name = 'sort'),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi, name = 'hi'),
+    path('posts/', post_views.list_post, name ='feed'),
+    path('users/login/', users_views.login_view, name ='login')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
