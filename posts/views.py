3b61ca359a4f54d23bfe8be35266ecc58b40cd78
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-08 14:49:58 -0500

    An new application called 'posts' and a the new route 'posts' were added.

---
+++ posts/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+
+from datetime import datetime
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Yésica Cortés',
+        'timestamp': datetime.now().strftime('%b %dth %Y - %H:%M hrs'),
+        'picture' : 'https://picsum.photos/200/200/?image=1036'
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth %Y - %H:%M hrs'),
+        'picture' : 'https://picsum.photos/200/200/?image=903'
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth %Y - %H:%M hrs'),
+        'picture' : 'https://picsum.photos/200/200/?image=1076'
+    }
+]
+
+def list_post(request):
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
