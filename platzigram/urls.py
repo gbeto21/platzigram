63da66cd36b8bc066cbe4a545d1c1068851d1a54
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-08 17:47:38 -0500

    A template to the posts page was created and the project was configured to serve statics files.

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -8,9 +8,11 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
     path('posts/', post_views.list_post)
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
