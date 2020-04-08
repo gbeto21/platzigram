c79b4a86fcf818dafd384b350b13d67043e79ecc
Alberto González Balderas <albertoglezb@hotmail.com>
2020-04-08 17:05:47 -0500

    The function of edith a profile and the picture was developed.

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,9 +1,11 @@
 """Platzigram URLS module."""
 
 from django.urls import path
+from django.conf.urls.static import static
 from platzigram import views as local_views
 from posts import views as post_views
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,4 @@ urlpatterns = [
     path('sorted/', local_views.sort_integers),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
     path('posts/', post_views.list_post)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
